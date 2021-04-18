edbe40fc00da040b1b7f6b24ab04d3edc89b174e
Florencia Lafuente <flor@properati.com>
2021-04-18 18:46:31 -0300

    delete title from _document

--- pages/_document.js
+++ pages/_document.js
@@ -23,7 +23,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>My page</title>
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
         </Head>
